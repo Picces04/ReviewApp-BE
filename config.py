34df0cff42b374460672309caa11c876229ece60
Picces04 <khanhhaivodoi@gmail.com>
2025-07-25 11:31:51 +0700

    update env

--- config.py
+++ config.py
@@ -11,3 +11,6 @@ MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
+
+#CORS
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from routes.auth import auth_router
 from routes.items import items_router
 from routes.likes import likes_router
 from routes.cloudinary_api import cloudinary_router
+from config import FRONTEND_URL
 
 app = FastAPI()
 
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+     allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
